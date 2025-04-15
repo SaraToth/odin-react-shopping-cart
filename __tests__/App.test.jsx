6d9d03b92b4be662cc940cd2cfa8660a6c536412
SaraToth <saratoth101@gmail.com>
2025-04-16 08:40:44 +0900

    Test: Remove unused imports from App Test

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,6 +1,4 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../src/App";
 
 describe('something truthy and falsy', () => {
     it('true to be true', () => {
