5c8d06b567bb7de848892c6f18b81dd456413866
SaraToth <saratoth101@gmail.com>
2025-04-16 08:27:41 +0900

    Feat: Import module css into Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <h1>hello</h1>
+    <Navbar cartNumber={0}/>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-
+import styles from "./Navbar.module.css"
 const Navbar = ({cartNumber}) => {
 
     return (
-        <nav>
+        <nav className={styles.nav}>
             <ul>
                 <li><Link to ="/">Home</Link></li>
                 <li><Link to="/Shop">Shop</Link></li>
