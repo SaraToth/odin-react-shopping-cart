9346dbd36c80c2d3672d42c40ab6bfa221ed694d
SaraToth <saratoth101@gmail.com>
2025-04-17 08:30:31 +0900

    Feat: Remove navbar from Shop page component
    Navbar will be universally loaded in App and Shop page does not need to contain it.

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,7 @@
-import Navbar from "../components/Navbar";
-
 const Shop = () => {
     return (
         <div>
             <h1>This is the Shop page.</h1>
-            <Navbar />
         </div>
 
     )
