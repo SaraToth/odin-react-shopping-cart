5988327587077c27e225daf5b9b5fd2d604fb63e
SaraToth <saratoth101@gmail.com>
2025-04-17 15:27:57 +0900

    Feat: Create a useEffect to fetch shop data
    Fetch shop data from API

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 const Shop = () => {
+    const [shopData, setShopData] = useState([]);
+
+    useEffect(() => {
+        async function fetchShopdata () {
+            const response = await fetch('https://fakestoreapi.com/products/');
+            const shopData = await response.json();
+            console.log(shopData);
+
+
+        }
+        fetchShopdata();
+    }, []);
+
     return (
         <h1>This is the Shop page.</h1>
     )
