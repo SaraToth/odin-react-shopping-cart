d9b1397681911a8dad2e05e16995f2b86f833f2b
SaraToth <saratoth101@gmail.com>
2025-04-17 21:37:20 +0900

    Test: Finish Shop test

--- __tests__/Shop.test.jsx
+++ __tests__/Shop.test.jsx
@@ -58,6 +58,7 @@ describe("Shop page", () => {
 
         const productTitle = await screen.findByText(/test product/i);
         const price = await screen.findByText(/19.99/i);
+        
         expect(productTitle).toBeInTheDocument();
         expect(price).toBeInTheDocument();
 
