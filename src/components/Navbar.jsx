1a00528501d6ce8c354944423094dd1130ab0ef5
SaraToth <saratoth101@gmail.com>
2025-04-17 17:00:49 +0900

    Feat: Add shopping cart img to Navbar and style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import cart from "../assets/shopping-cart.png";
 
 const Navbar = ({cartNumber}) => {
 
     return (
         <nav className={styles.nav}>
             <ul className={styles.ul}>
-                <li><Link to ="/">Home</Link></li>
-                <li><Link to="/Shop">Shop</Link></li>
+                <li><Link className={styles.link} to ="/">Home</Link></li>
+                <li><Link className={styles.link} to="/Shop">Shop</Link></li>
             </ul>
+            <span><img className={styles.cart} src={cart}/></span>
+            <span className={styles.badge}>3</span>
             {cartNumber > 0 && cartNumber + " items in cart"}
             {cartNumber === 0 && "Cart is empty"}
         </nav>
