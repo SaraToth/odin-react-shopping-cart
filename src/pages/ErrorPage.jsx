455d110f25e378af0715c9b7a16b35818681c039
SaraToth <saratoth101@gmail.com>
2025-04-17 21:34:16 +0900

    Feat: Simplify ErrorPage content

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom";
+
 
 const ErrorPage = () => {
     return (
-        <>
+        <div>
         <p>There was a network issue:</p>
-        <Link to="/">Return to the main page</Link>
-        </>
+        </div>
     )
 };
 
