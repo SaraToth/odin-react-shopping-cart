11c4da86a4224e8e14e41be3b30b82b0a8bbeaea
SaraToth <saratoth101@gmail.com>
2025-04-17 18:06:24 +0900

    Style: Name store "Daywish Stationary"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import styles from "./Home.module.css";
 const Home = () => {
     return (
         <section className={styles.section}>
-            <h1><b><i>Shop Name</i></b></h1>
+            <h1><b><i>Daywish Stationary</i></b></h1>
             <Link className={styles.linkButton} to="/shop">Shop Now</Link>
         </section>
     )
