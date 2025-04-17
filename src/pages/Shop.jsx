bd6e91b77c6809c626c5da519460d952541bdfec
SaraToth <saratoth101@gmail.com>
2025-04-17 15:41:04 +0900

    Feat: Refine API call to filter data
    Update state only with the data that is needed

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,15 +4,32 @@ const Shop = () => {
     const [shopData, setShopData] = useState([]);
 
     useEffect(() => {
-        async function fetchShopdata () {
+        async function fetchShopData () {
             const response = await fetch('https://fakestoreapi.com/products/');
             const shopData = await response.json();
-            console.log(shopData);
+            
+            const filteredData = [];
 
+            shopData.map((item) => {
+                const filteredItem = createDataObject(item);
+                filteredData.push(filteredItem);
+            })
 
+            setShopData(filteredData);
         }
-        fetchShopdata();
+
+        fetchShopData();
     }, []);
+
+    const createDataObject = (item) => {
+        return {
+            imageURL: item.image,
+            price: item.price,
+            title: item.title,
+            id: item.id,
+            description: item.description,
+        }
+    }
 
     return (
         <h1>This is the Shop page.</h1>
