7b54e0a37a2043cee48bb114fc233f4fb091e9d0
SaraToth <saratoth101@gmail.com>
2025-04-17 17:15:53 +0900

    Feat: Convert cart text to a cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import cart from "../assets/shopping-cart.png";
 
-const Navbar = ({cartNumber}) => {
+const Navbar = ({cartNumber = 2}) => {
 
     return (
         <nav className={styles.nav}>
@@ -10,10 +10,8 @@ const Navbar = ({cartNumber}) => {
                 <li><Link className={styles.link} to ="/">Home</Link></li>
                 <li><Link className={styles.link} to="/Shop">Shop</Link></li>
             </ul>
-            <span><img className={styles.cart} src={cart}/></span>
-            <span className={styles.badge}>3</span>
-            {cartNumber > 0 && cartNumber + " items in cart"}
-            {cartNumber === 0 && "Cart is empty"}
+            <span><img alt="cart" className={styles.cart} src={cart}/></span>
+            {cartNumber > 0 && <span className={styles.badge}>{cartNumber}</span>}
         </nav>
     )
 }
