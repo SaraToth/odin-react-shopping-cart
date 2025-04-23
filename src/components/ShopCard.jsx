aa5bbcde1a068a41a6b756035d3573eb4dd25375
SaraToth <saratoth101@gmail.com>
2025-04-24 08:53:38 +0900

    Docs: Create custom types for ShopCard Item

--- src/components/ShopCard.jsx
+++ src/components/ShopCard.jsx
@@ -1,6 +1,14 @@
 import { useOutletContext } from "react-router-dom";
 import styles from "./ShopCard.module.css";
 
+/**
+ * Renders a product card with title, price, image, and add to cart button
+ * 
+ * @typedef {import("../utils/types.jsdoc").ShopItem} ShopItem
+ * 
+ * @param {{ shopItem: ShopItem }} props
+ * @returns {JSX.Element}
+ */
 const ShopCard = ({shopItem}) => {
     const {cartNumber, setCartNumber} = useOutletContext();
 
---
+++ src/utils/types.jsdoc.js
@@ -0,0 +1,12 @@
+/**
+ * @module Types
+ */
+
+/**
+ * @typedef {Object} ShopItem
+ * @property {string} imageURL
+ * @property {number} price
+ * @property {string} title
+ * @property {number} id
+ * @property {string} description
+ */
