3ba916a80c89929fe38d746e38a5cc9f39c21f8e
SaraToth <saratoth101@gmail.com>
2025-04-23 14:58:52 +0900

    Refactor: Shop API fetch catches error in function
    Refactor the chained catch to the API fetch function call to a try/catch block to handle the error internally, and update component to render the ErrorPage component for errors

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import ShopCard from "../components/ShopCard";
 import styles from "./Shop.module.css";
 import { useOutletContext } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 
 const Shop = () => {
     const [shopData, setShopData] = useState([]);
@@ -10,29 +11,36 @@ const Shop = () => {
 
     useEffect(() => {
         async function fetchShopData () {
-            const response = await fetch('https://fakestoreapi.com/products/');
-            const rawShopData = await response.json();
-            
-            const filteredData = [];
+            try {
+                const response = await fetch('https://fakestoreapi.com/roducts/', { mode: "cors"});
 
-            rawShopData.map((rawData) => {
-                const item = {
-                    imageURL: rawData.image,
-                    price: rawData.price,
-                    title: rawData.title,
-                    id: rawData.id,
-                    description: rawData.description,
-                };
+                if(response.status >= 400) {
+                    throw new Error("server error");
+                }
 
-                filteredData.push(item);
-            })
+                const rawShopData = await response.json();
+                const filteredData = [];
+                
+                rawShopData.map((rawData) => {
+                    const item = {
+                        imageURL: rawData.image,
+                        price: rawData.price,
+                        title: rawData.title,
+                        id: rawData.id,
+                        description: rawData.description,
+                    };
+    
+                    filteredData.push(item);
+                })
+    
+                setShopData(filteredData);
 
-            setShopData(filteredData);
+            } catch (error) {
+                setError(error);
+            }
         }
 
-        fetchShopData().catch((error) => {
-            setError(error);
-        });
+        fetchShopData()
     }, []);
 
 
@@ -41,7 +49,7 @@ const Shop = () => {
             <span className={styles.span}><h1>SHOP</h1></span>
 
             <div className={styles.shopGrid}>
-                {error && <p>A network error occured</p>}
+                {error && <ErrorPage />}
                 {shopData &&  shopData.map((shopItem) => {
                    return ( <ShopCard cartNumber={cartNumber} setCartNumber={setCartNumber} shopItem={shopItem} key={shopItem.id} />)
                 })}
