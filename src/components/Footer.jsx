43e4461c014ef546f82e13ce86cf6bd0a9df1dc6
SaraToth <saratoth101@gmail.com>
2025-04-23 19:01:40 +0900

    Docs: Update JSDoc for Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 import styles from "./Footer.module.css";
 
+/**
+ * Footer Component
+ * 
+ * @returns {JSX.ELement} Renders Footer content
+ */
 const Footer = () => {
     return  (
         <footer className={styles.footer}>Sara Toth &#169; 2025. All rights reserved.</footer>
