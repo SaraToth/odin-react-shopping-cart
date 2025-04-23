dcf69fad6d50cf6b9a02cdb8af3f581db92fd2f3
SaraToth <saratoth101@gmail.com>
2025-04-23 18:58:53 +0900

    Docs: Write Home component JSDOC

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
+/**
+ * Home Page Component - Renders home page content
+ * 
+ * @returns {JSX.Element} 
+ */
+
 const Home = () => {
     return (
         <section className={styles.section}>
