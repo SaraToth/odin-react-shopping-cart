535c95c614f40d4cde2bc12f100f9f876e5e7ac2
SaraToth <saratoth101@gmail.com>
2025-04-26 18:06:54 +0900

    Feat: Preload Home page background img with effect
    Preload home image improves first content paint

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
+import bgdImage from "../../assets/notebooks-bgd.webp";
+import { useEffect } from "react";
 
 /**
  * Renders Home page text and shop now button
@@ -7,8 +9,21 @@ import styles from "./Home.module.css";
  * @returns {JSX.Element}
  */
 const Home = () => {
+    useEffect(() => {
+        const link = document.createElement("link");
+        link.rel = "preload";
+        link.as = "image";
+        link.href = bgdImage;
+        link.type = "image/webp";
+        document.head.appendChild(link);
+
+        return () => {
+            document.head.removeChild(link); //Cleanup if component unmounts
+        }
+    }, []);
+
     return (
-        <section className={styles.section}>
+        <section className={`${styles.section} ${styles.bgdImage}`}>
             <h1><b><i>Daywish Stationary</i></b></h1>
             <Link className={styles.linkButton} to="/shop">Shop Now</Link>
         </section>
