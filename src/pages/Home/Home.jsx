055a02b14292073d899966286616ab88d6b1857c
SaraToth <saratoth101@gmail.com>
2025-04-26 18:31:50 +0900

    Feat: Convert img preload to layoutEffect

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 import bgdImage from "../../assets/notebooks-bgd.webp";
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 
 /**
  * Renders Home page text and shop now button
@@ -10,7 +10,7 @@ import { useEffect } from "react";
  */
 const Home = () => {
     // Preloads background image
-    useEffect(() => {
+    useLayoutEffect(() => {
         const link = document.createElement("link");
         link.rel = "preload";
         link.as = "image";
